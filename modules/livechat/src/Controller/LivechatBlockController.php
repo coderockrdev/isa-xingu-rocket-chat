0e10d54b546a96541ebe011f197d5219292d4919
Lawri van Buël <sysosmaster@2588960.no-reply.drupal.org>
2017-08-29 14:06:43 +0200

    Re-added a template that mistakenly was dealeted during module code cleanup.

--- modules/livechat/src/Controller/LivechatBlockController.php
+++ modules/livechat/src/Controller/LivechatBlockController.php
@@ -14,4 +14,35 @@ class LivechatBlockController {
     return 'livechat';
   }
 
+    /**
+   * Generate a render array with our templated content.
+   *
+   * @return array
+   *   A render array.
+   */
+  public function description() {
+    $template_path = $this->getDescriptionTemplatePath();
+    $template = file_get_contents($template_path);
+    $build = [
+      'description' => [
+        '#type' => 'inline_template',
+        '#template' => $template,
+        '#context' => $this->getDescriptionVariables(),
+      ],
+    ];
+    return $build;
+  }
+
+  /**
+   * Get full path to the template.
+   *
+   * @return string
+   *   Path string.
+   */
+  protected function getDescriptionTemplatePath() {
+
+    return drupal_get_path('module', $this->getModuleName()) . "/templates/description.html.twig";
+  }
+
+
 }
